9e4652a9485bee236988fee9e038865508b69b54
O3-Shop <o3-shop-github@ist-einmalig.de>
2023-03-10 21:08:01 +0100

    remove compiled files, adjust configuration

--- source/conf.py
+++ source/conf.py
@@ -6,9 +6,17 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'O3-Shop Documentation'
-copyright = '2023, O3-Shop Community'
+import datetime as dt
+
+project = 'O3-Shop'
+copyright = '2023 - {}, O3-Shop, 2021 - 2022 OXID eSales AG'.format(dt.date.today().year)
 author = 'O3-Shop Community'
+
+# The short X.Y version
+version = '1.0'
+# The full version, including alpha/beta/rc tags
+release = '1.0.0'
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -28,8 +36,32 @@ html_static_path = ['_static']
 myst_enable_extensions = ["colon_fence"]
 
 html_logo = 'assets/logo_light.png'
-display_gitlab = True
-gitlab_url = 'https://gitlab.o3-shop.com/o3/o3-documentation'
+html_favicon = 'assets/favicon.ico'
+# html_css_files = [
+#     'css/o3.css',
+# ]
+
+html_context = {
+    'current_version': '1.0',
+    'versions':
+        [
+            ('1.0', 'https://docs.o3-shop.com/en/1.0/')
+        ],
+    'languages':
+        [
+            ('EN', 'https://docs.o3-shop.com/en/1.0/')
+        ],
+    
+    'show_sphinx': False,
+    
+    "display_gitlab": True, # Integrate Gitlab
+    "gitlab_host": "gitlab.o3-shop.com",
+    "gitlab_user": "o3", # Username
+    "gitlab_repo": "o3-documentation", # Repo name
+    "gitlab_version": "main", # Version
+    "conf_py_path": "/source/", # Path in the checkout to the docs root
+}
+
 html_theme_options = {
     'logo_only': True,
     'display_version': True,
